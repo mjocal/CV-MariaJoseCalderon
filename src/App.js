3ffb0129ef645e4574b03cda893054128569d8eb
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-24 23:31:26 -0600

    initial page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { StyledApp, StyledH1 } from './styles'
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<StyledApp>
+			{/* <BodyFlow /> */}
+			<div className='content-wrap'>
+				<div className='app-header'>
+					<StyledH1>Finsol PMO+ HUB</StyledH1>
+				</div>
+				<div className='app-content'>
+					{/* LOCAL TESTING */}
+					<h1>Test</h1>
+					{/* <Router basename="/pmoHub">
+            <LandingMenu path={`/`} />
+            <ReportingMenu path={`/Reporting`} />
+            <CapacityMenu path={`/CapacityCheck`} />
+          </Router> */}
+				</div>
+			</div>
+			<div className='app-footer'>
+				{/* <Footer /> */}
+				<h3>Test footer</h3>
+			</div>
+		</StyledApp>
 	)
 }
 
