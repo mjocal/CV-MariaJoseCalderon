3bba2cd1413960310c97cee469213f7dcd79cb71
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-26 10:04:40 -0600

    created about me component

---
+++ src/components/AboutMe/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledH = styled.h1`
+	font-size: 2.8rem;
+	font-weight: 500;
+	color: #fff;
+	text-align: center;
+	text-shadow: 2px 2px #395562;
+	margin: 0 auto;
+`
+
+export default StyledH
