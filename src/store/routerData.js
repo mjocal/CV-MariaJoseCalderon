e3e1877e8c2b5fb5693fb76fe47a222a94c63a90
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-26 10:06:05 -0600

    Added routing to the app

---
+++ src/store/routerData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Router } from '@reach/router'
+import Homepage from '../components/Homepage'
+
+const RouterApp = (
+	<Router>
+		<Homepage path='/' />
+	</Router>
+)
+
+export default RouterApp
---
+++ src/store/urlData.js
@@ -0,0 +1,25 @@
+const UrlData = {
+	homeUrl: `/`,
+	aboutMeUrl: `/aboutMe`,
+	aboutPageUrl: `/aboutPage`,
+	linkedInUrl:
+		'https://www.linkedin.com/in/mar%C3%ADa-jos%C3%A9-calder%C3%B3n-51448050/',
+	gitHubUrl: 'https://github.com/mjocal',
+	// capacityMenuUrl: `/CapacityCheck`,
+
+	// PRODUCTION
+	// homeUrl: `${process.env.PUBLIC_URL}/`,
+	// landingMenuUrl: `${process.env.PUBLIC_URL}/landingMenus`,
+	// reportingMenuUrl: `${process.env.PUBLIC_URL}/reportingMenus`,
+	// capacityMenuUrl: `${process.env.PUBLIC_URL}/capacityMenus`,
+
+	// portfolioUrl: ("https://finsolcr.pg.com/pmoPortfolio"),
+	// pmWizardUrl: "https://finsolcr.pg.com/pmWizard/",
+	// finantialManagementUrl: "",
+	// resourcesManagementUrl: "",
+	// capabilityUrl: "https://pgone.sharepoint.com/sites/OnboardingTrainingPackageProcess/SitePages/Principal.aspx",
+	// projectAutomationUrl: "https://pgone.sharepoint.com/:u:/s/FSSTestingAutomationService/EeqBt3yY40ZMp2zUWda1GIIBHtHrfJwSc8fftU9EvLG-lg?e=EK5wb0",
+	// vendorManagementUrl: "",
+}
+
+export default UrlData
