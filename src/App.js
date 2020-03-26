380dde468c2168abc3f497b9c6ebaabb3993a29c
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-26 10:06:50 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { StyledApp, StyledH1, StyledH2 } from './styles'
 import Background from './components/Background'
+import Homepage from './components/Homepage'
+import AboutMe from './components/AboutMe'
+import AboutPage from './components/AboutPage'
 import Footer from './components/Footer'
 
 function App() {
@@ -13,13 +17,11 @@ function App() {
 					<StyledH2>Software Developer</StyledH2>
 				</div>
 				<div className='app-content'>
-					{/* LOCAL TESTING */}
-					<h1>Test</h1>
-					{/* <Router basename="/pmoHub">
-            <LandingMenu path={`/`} />
-            <ReportingMenu path={`/Reporting`} />
-            <CapacityMenu path={`/CapacityCheck`} />
-          </Router> */}
+					<Router>
+						<Homepage path={`/`} />
+						<AboutMe path={`/aboutMe`} />
+						<AboutPage path={`/aboutPage`} />
+					</Router>
 				</div>
 			</div>
 			<div className='app-footer'>
