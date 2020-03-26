f10df944752eeb07539f94983173012ae0d71c4a
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-26 10:04:23 -0600

    created about page component

---
+++ src/components/AboutPage/index.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react'
+import { StyledH } from './styles'
+import { StyledButton } from '../../styles'
+import { navigate } from '@reach/router'
+import UrlData from '../../store/urlData'
+
+const AboutPage = () => {
+	// let key = 1
+
+	const handleClick = () => {
+		navigate(UrlData.homeUrl)
+	}
+
+	return (
+		<Fragment>
+			{/* <StyledCardsDiv className="wrapper">
+            <div className="cols">
+            {cardData.map(data => (
+                <Card
+                key={`ck-${key++}`}
+                title={data.title}
+                description={data.description}
+                isExternal={data.isExternal}
+                imageUrl={data.imageUrl}
+                pageUrl={data.pageUrl}
+                bgColor1={data.bgColor1}
+                bgColor2={data.bgColor2}
+                />
+            ))}
+            </div>
+        </StyledCardsDiv> */}
+
+			<StyledH>About Page Test</StyledH>
+
+			<StyledButton onClick={handleClick}>Back to home</StyledButton>
+		</Fragment>
+	)
+}
+
+export default AboutPage
---
+++ src/components/AboutPage/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledH = styled.h1`
+	font-size: 2.8rem;
+	font-weight: 500;
+	color: #fff;
+	text-align: center;
+	text-shadow: 2px 2px #395562;
+	margin: 0 auto;
+`
