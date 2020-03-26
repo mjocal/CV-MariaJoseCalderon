26eed16a302f5013eee26775314bfc25da11ab47
Majo Calderon <dra.mariacalderon@gmail.com>
2020-03-26 10:06:23 -0600

    updated styles

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,22 +1,15 @@
 import styled from 'styled-components'
 import globalStyles from '../../store/globalStyles'
 
-const {
-	globalFont,
-	footerBgColor,
-	footerTextColor,
-	footerTextShadow,
-} = globalStyles
+const { globalFont } = globalStyles
 
 const StyledFooter = styled.div`
 	.footer-content {
 		font-family: ${globalFont};
-		/* background-color: ${footerBgColor}; */
 		font-size: 15px !important;
 		text-align: center;
 		color: black;
 		text-shadow: grey;
-		/* padding-bottom: 10px; */
 	}
 `
 
