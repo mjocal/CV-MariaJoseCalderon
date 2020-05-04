637eaee6090301ba7daf0aba483ffb0cf713bb5f
Majo Calderon <dra.mariacalderon@gmail.com>
2020-05-04 02:00:10 -0600

    Updates

---
+++ precache-manifest.0c244e7384e6f79e76a0ca07307b590f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f325dcb9915e7e031da964d9e9157e1",
+    "url": "/cv_mjocal/index.html"
+  },
+  {
+    "revision": "f06c84e5e66be306ad37",
+    "url": "/cv_mjocal/static/js/2.2cc4f6da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv_mjocal/static/js/2.2cc4f6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df50ed09a6f7cce20aa7",
+    "url": "/cv_mjocal/static/js/main.b0a0915a.chunk.js"
+  },
+  {
+    "revision": "3d9677160c41e62c8e80",
+    "url": "/cv_mjocal/static/js/runtime-main.15b0b1de.js"
+  },
+  {
+    "revision": "e228f1536197e2e64db7bac5dfe567b8",
+    "url": "/cv_mjocal/static/media/CV_MaJose_Calderon_SoftwareDeveloper.e228f153.pdf"
+  }
+]);
