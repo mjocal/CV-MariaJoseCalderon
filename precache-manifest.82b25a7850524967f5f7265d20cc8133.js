e66e2a1cbdc2498a8bfe6dfac47ed220f8ab36d7
Majo Calderon <dra.mariacalderon@gmail.com>
2020-05-04 02:46:16 -0600

    Updates

--- precache-manifest.82b25a7850524967f5f7265d20cc8133.js
+++ precache-manifest.82b25a7850524967f5f7265d20cc8133.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "80cf7cff847e8edbf038ca49d3a942f0",
+    "revision": "364a1ac09266770593291286d583e450",
     "url": "/cv_mjocal/index.html"
   },
   {
-    "revision": "f06c84e5e66be306ad37",
-    "url": "/cv_mjocal/static/js/2.2cc4f6da.chunk.js"
+    "revision": "afae7d3db5a4d0b37f55",
+    "url": "/cv_mjocal/static/js/2.7ded341e.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/cv_mjocal/static/js/2.2cc4f6da.chunk.js.LICENSE.txt"
+    "url": "/cv_mjocal/static/js/2.7ded341e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f60d43bb799edb0b61f",
-    "url": "/cv_mjocal/static/js/main.771a4585.chunk.js"
+    "revision": "563ab0d66548537b5a9e",
+    "url": "/cv_mjocal/static/js/main.cd9f36eb.chunk.js"
   },
   {
     "revision": "3d9677160c41e62c8e80",
@@ -22,5 +22,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e228f1536197e2e64db7bac5dfe567b8",
     "url": "/cv_mjocal/static/media/CV_MaJose_Calderon_SoftwareDeveloper.e228f153.pdf"
+  },
+  {
+    "revision": "a7cfeee2fd645aa27c31bad04ed5ef6e",
+    "url": "/cv_mjocal/static/media/bananaCake.a7cfeee2.jpg"
   }
 ]);
